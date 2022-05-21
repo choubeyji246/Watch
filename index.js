64bf6b43cf920330cef5813286f87211dcb4171a
ankitchoubey7504270771@gmail.com <ankitchoubey7504270771@gmail.com>
2022-05-21 10:03:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+setInterval(()=>{
+    a=new Date();
+    date=a.toLocaleDateString();
+    time=a.getHours()+":"+a.getMinutes()+":"+a.getSeconds();
+    document.getElementById("time").innerHTML=time +" on "+ date;
+},1000);
